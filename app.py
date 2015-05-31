2ac14464089d1b5f3feb6f484b9df7b77bf2a2d1
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2015-05-31 16:15:02 -0700

    search functionality v.0.0.1

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
 import os
 
-
 from flask import Flask, request, Response, redirect
+import giphypop
+from giphypop import translate
 app = Flask(__name__)
 
+key = 'dc6zaTOxFJmzC'
+g = Giphy(api_key=key, strict=True)
 
-@app.route("/")
+responses = [
+    "That sucks...",
+    "Can't find shit, sorry man..."
+]
+
+@app.route('/')
 def hello():
     return redirect('https://github.com/gtkesh/hedwig')
+
+
+@app.route('/hedwig', methods=['post'])
+def hedwig():
+    keyword = request.values.get('text')
+    gif     = translate(keyword)
+
+    try:
+        return g.random_gif(keyword).media_url
+    except GiphyApiException:
+        try:
+            return g.translate(keyword).media_url
+        except GiphyApiException:
+            try:
+                g.search_list(keyword, limit=1)[0].media_url
+            except (GiphyApiException, IndexError):
+                    pass
+
+
+    return random.choice(responses)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
