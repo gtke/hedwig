b54c230500257076b5542185329f389e6ae67923
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2015-05-31 15:00:05 -0700

    added OS.env

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ def hello():
     return redirect('https://github.com/gtkesh/hedwig')
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port)
