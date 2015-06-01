1980258d51de6a1eb864c86436e107a9f0f490d6
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2015-05-31 17:25:33 -0700

    ss

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+import logging
 from flask import Flask, request, Response, redirect, send_file
 import giphypop
 from giphypop import translate
@@ -18,28 +18,28 @@ def hello():
     return redirect('https://github.com/gtkesh/hedwig')
 
 
-@app.route('/hedwig', methods=['post'])
+@app.route('/hedwig', methods=['get, post'])
 def hedwig():
     keyword = request.values.get('text')
+    logging.info('passed keyword', keyword)
     gif     = translate(keyword)
-
+    filename = 'https://media1.giphy.com/media/fAT2Db0j0Mblu/200_s.gif'
     try:
         filename = g.random_gif(keyword).media_url
-        return send_file(filename, mimetype='image/gif')
     except GiphyApiException:
         try:
             filename = g.translate(keyword).media_url
-            return send_file(filename, mimetype='image/gif')
         except GiphyApiException:
             try:
                 filename = g.search_list(keyword, limit=1[0]).media_url
-                return send_file(filename, mimetype='image/gif')
             except (GiphyApiException, IndexError):
                     pass
 
-
-    return random.choice(responses)
-
+    return  '''
+        <!doctype html>
+        <title>Hello from Hedwig</title>
+        <img src=filename>
+    '''
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
